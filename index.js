7e25ce270df12921163c97484422def82dc5a5fc
Omar Gonzalez <ogonzaleznyc@gmail.com>
2019-09-29 01:51:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,18 +1,33 @@
+// Code your solution here:
+
+
 function driversWithRevenueOver(arrayOfDriverObjects, number) {
-  return arrayOfDriverObjects.filter(driver => driver.revenue > number);
+  return arrayOfDriverObjects.filter(driverObject => driverObject.revenue > number);
 }
 
 
-function driverNamesWithRevenueOver(arrayOfDriverObjects, number) {
-  return driversWithRevenueOver(arrayOfDriverObjects, number).map(driver => driver.name);
+function driverNamesWithRevenueOver(array, number) {
+  return driversWithRevenueOver(array, number).map(driverObject => driverObject.name);
 }
 
 
-function exactMatch(arrayOfDriverObjects, attributeValueObject) {
-  return arrayOfDriverObjects.filter(driver => Object.values(driver).includes(Object.values(attributeValueObject)[0]) && driver[attributeValueObject[0]] === attributeValueObject[attributeValueObject[0]]
-)}
+// function exactMatch(arrayOfDriverObjects, unknownObject) {
+//   return arrayOfDriverObjects.filter(driverObject => Object.keys(driverObject).includes(Object.keys(unknownObject)[0]) && 
+//   driverObject[Object.keys(unknownObject)[0]] === unknownObject[Object.keys(unknownObject)[0]]
+//   );
+// }
+
+function exactMatch (arrayOfDriverObjects, unknownObject) {
+  return arrayOfDriverObjects.filter(function(driverObject) {
+    let matches
+    for (const key in unknownObject) {
+      matches = driverObject[key] === unknownObject[key]
+    }
+    return matches
+  })
+}
 
 
-function exactMatchToList(arrayOfDriverObjects, attributeValueObject) {
-  return exactMatch(arrayOfDriverObjects, attributeValueObject).map(driver => driver.name);
+function exactMatchToList(arrayOfDriverObjects, object) {
+  return exactMatch(arrayOfDriverObjects, object).map(driverObject => driverObject.name);
 }
